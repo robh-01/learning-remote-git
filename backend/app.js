40a549baf5a46b9fd081a5cd412799f238fbf247
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-06-07 12:16:32 +0545

    Emptied

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get("", (req, res) => {
-  res.send("Hello");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
