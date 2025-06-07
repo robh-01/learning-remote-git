00d39ebb351a26ea8d24b28636b8dce36d00dfa4
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-06-07 13:31:11 +0545

    Started fronten 2d

---
+++ frontend/src/Components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import styles from "./Body.module.css";
+
+function Body() {
+  return (
+    <>
+      <div className={styles.cont}>This is body</div>
+      
+    </>
+  );
+}
+
+export default Body;
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className={styles.nav}>Hello there</div>
+      
+    </>
+  );
+}
+
+export default Navbar;
